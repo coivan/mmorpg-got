08bd759511dcad4ad4112856f1c92ffb95e46442
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2022-05-05 09:05:52 -0300

    Controller de autenticação

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,3 +3,9 @@ module.exports = function(application){
 		application.app.controllers.index.index(application, req, res);
 	});
 }
+
+module.exports = function(application){
+	application.post('/autenticar', function(req, res){
+		application.app.controllers.index.autenticar(application, req, res);
+	});
+}
